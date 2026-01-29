a9370ab401dd56b6e3c438021cc316a0b272b4f3
Yashwant0706 <yashwantjaybhaye7@gamil.com>
2026-01-30 00:30:44 +0530

    New Project ATM Interface

--- ATM_Inerface.java
+++ ATM_Inerface.java
@@ -1,32 +1,32 @@
+
 import java.util.*;
 
 //----Class that manages the user's account details----//
-class UserAccount{
-   private double currentBalance;
+class UserAccount {
 
-   //----Constructor to set the initial balance----//
-   UserAccount(double initialBalance){
-      this.currentBalance = initialBalance;
-   }
+    private double currentBalance;
 
-   //----deposit money to the account----//
-   public void depositMoney(double amount){
-      if(amount>0){
-         currentBalance += amount;
-         System.out.println("Deposit completed sucessfully.");
-         }
-      else{
+    //----Constructor to set the initial balance----//
+    UserAccount(double initialBalance) {
+        this.currentBalance = initialBalance;
+    }
+
+    //----deposit money to the account----//
+    public void depositMoney(double amount) {
+        if (amount > 0) {
+            currentBalance += amount;
+            System.out.println("Deposit completed sucessfully.");
+        } else {
             System.out.println("Enter a valid amount to deposit.");
-         }
+        }
+        //----Withdrawal money from the account----//
+    public void withdrawalMoney(double amount){
+        if (amount > currentBalance) {
+            System.out.println("Transaction failed : Insufficient funds.");
+        } else if {
+            System.out.println()
+        }
+    }
 
-      //----Withdraws money from the account----//
-         public void witdraMoney(double amount){
-            if(amount > currentBlance){
-               System.out.println("Transaction failed : Insufficient funds.");
-            }
-            else
-         }
-
-   }
 }
-
+}
